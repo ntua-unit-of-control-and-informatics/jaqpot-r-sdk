b5f9e20da8fa61bd304789efea0cb0a66c4f567b
Periklis_ts <ptsirostsib@gmail.com>
2020-12-07 16:34:38 +0200

    Documentation ok

--- R/deploy_lm_glm.R
+++ R/deploy_lm_glm.R
@@ -3,12 +3,12 @@
 #' Uploads trained linear and generalized linear models on Jaqpot given
 #' an "lm" or "glm" object.
 #'
-#' @param object An object of either class "lm" (base function \code{lm()}) or "glm"
+#' @param object An object of class "lm" (base function \code{lm()}) or "glm"
 #' (base function \code{glm()}).
 #' @return  The id of the uploaded model.
 #' @details The user can upload on Jaqpot a model that has been trained using the base
 #'  functions \code{lm()} or \code{glm()}. Apart from the model object, the user is requested
-#'  to provide further information (i.e. Jaqpot API key or credentials, model title, short
+#'  to provide further information (i.e. Jaqpot API key or credentials, model title and short
 #'  description) via prompt messages. If the upload process is successful,
 #'  the user is given a unique model id key.
 #'
