b7d3e0ce5a551baab94adcd9fa5f0235c9314c77
Periklis_ts <ptsirostsib@gmail.com>
2020-12-18 14:05:05 +0200

    Update deploy_party.R

--- R/deploy_party.R
+++ R/deploy_party.R
@@ -59,7 +59,12 @@ deploy.party <- function(object, url = "https://api.jaqpot.org/"){
                  predictedFeatures=dependent.vars, dependentFeatures=dependent.vars,
                  title=title, description=description, algorithm="R/tree")
   # Convert the list to a JSON data format
-  json <- jsonlite::toJSON(tojson)
+  tryCatch({
+    json <- jsonlite::toJSON(tojson)
+    }, error = function(e) {
+          e$message <-"Failed to convert object to json. "
+          stop(e)
+    })
   # Function that posts the model on Jaqpot
   .PostOnService(base.path, token, json)
 }
