e768d1f9b5bba06f7d4a7291071c67c19e628d44
Periklis_ts <ptsirostsib@gmail.com>
2021-06-20 12:09:13 +0300

    Update deploy_caret.R

--- R/deploy_caret.R
+++ R/deploy_caret.R
@@ -3,7 +3,8 @@
 #' Uploads trained decision tree models on Jaqpot given
 #' a "tree" object.
 #'  
-#' @param preprocess The preprocess model 
+#' @param preprocess.model The preprocess model. Here it is very important not to include the response into the preprocessing phase!
+#' Allowed types of preprocessing: 'preProcess' and 'dummyVars' caret functions. 
 #' @param trained.model An object of class "tree" (function \code{ctree(()} of package 'party'). 
 #' @param url The base path of Jaqpot services. This argument is optional and needs 
 #' to be changed only if an alternative Jaqpot installation is used.
@@ -22,7 +23,7 @@
 #' }
 #'
 #' @export
-deploy.caret <- function(preprocess =NULL, trained.model, url = "https://api.jaqpot.org/jaqpot/"){
+deploy.caret <- function(preprocess.model =NULL, trained.model, url = "https://api.jaqpot.org/jaqpot/"){
   
   # Get trained.model class
   #obj.class <- attributes(trained.model)$class[1] # class of tree models is "tree"
@@ -44,21 +45,39 @@ deploy.caret <- function(preprocess =NULL, trained.model, url = "https://api.jaq
   description <- readline("Short description of the model: ")
   
   # Obtain the library on which the model was built
-  library <- fitXGB2$modelInfo$library
+  library <- trained.model$modelInfo$library
   
-  #Retrieve the independent vars
-  independent.vars <- array(attributes(trained.model$terms)$term.labels)
-  # Number of responses
-  N_resp <-  attributes(trained.model$terms)$response
-  # All variables (dependent and independent)
-  all_vars_init <- as.character(attributes(trained.model$terms)$variables)
-  all_vars <- all_vars_init[2:length(all_vars_init)]
-  # Retrieve predicted variables by using set difference
-  dependent.vars <- setdiff(all_vars, independent.vars)
-  
+  if (!is.null(preprocess.model)){
+    if(attributes(preprocess.model[[1]])$class == "dummyVars"){
+      independent.vars <- preprocess.model[[1]]$vars
+    }else if (attributes(preprocess.model[[1]])$class == "preProcess"){
+      # Obtain the names of the variables from the variables on the method used, combined with the ignored variables 
+      independent.vars <- c(preprocess.model[[1]]$method[[1]],preprocess.model[[1]]$method[[2]])
+    }
+    #Retrieve the model independent vars (may contain variables resulting from one-hot encoding)
+    model.independent.vars <- array(attributes(trained.model$terms)$term.labels)
+    # Number of responses
+    N_resp <-  attributes(trained.model$terms)$response
+    # All variables (dependent and independent)
+    all_vars_init <- as.character(attributes(trained.model$terms)$variables)
+    all_vars <- all_vars_init[2:length(all_vars_init)]
+    # Retrieve predicted variables by using set difference
+    dependent.vars <- setdiff(all_vars, independent.vars)
+    
+    }else{
+    #Retrieve the independent vars
+    independent.vars <- array(attributes(trained.model$terms)$term.labels)
+    # Number of responses
+    N_resp <-  attributes(trained.model$terms)$response
+    # All variables (dependent and independent)
+    all_vars_init <- as.character(attributes(trained.model$terms)$variables)
+    all_vars <- all_vars_init[2:length(all_vars_init)]
+    # Retrieve predicted variables by using set difference
+    dependent.vars <- setdiff(all_vars, independent.vars)
+  }
  
   # Serialize the model in order to upload it on Jaqpot
-  model <- serialize(list(MODEL=trained.model, PREPROCESS = preprocess),connection=NULL)
+  model <- serialize(list(MODEL=trained.model, PREPROCESS = preprocess.model),connection=NULL)
   
   # Create a list containing the information that will be uploaded on Jaqpot
   tojson <- list(rawModel=model, runtime="R-caret", implementedWith="caret  R",
