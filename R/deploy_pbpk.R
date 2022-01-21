86227ba83ace97c5c3bef38c33465266f7d633d8
Periklis_ts <ptsirostsib@gmail.com>
2022-01-21 18:08:25 +0200

    Update deploy_pbpk.R

--- R/deploy_pbpk.R
+++ R/deploy_pbpk.R
@@ -181,7 +181,7 @@
 #' @export
 
 deploy.pbpk <- function(user.input, out.vars, create.params, create.inits, 
-                       create.events,custom.fun, ode.fun, method = "lsodes",
+                       create.events,custom.func, ode.fun, method = "lsodes",
                        url = "https://api.jaqpot.org/jaqpot/", ...){
   # Read the base path from the reader
     base.path <- url
