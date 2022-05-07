e8f2f9208a25c5cc8da811c68e50d75faf48b502
Periklis_ts <ptsirostsib@gmail.com>
2022-05-07 19:55:55 +0300

    Update login.R

--- R/login.R
+++ R/login.R
@@ -14,27 +14,28 @@
 #' @export
 
 login.cred.safe <- function(url = "https://api.jaqpot.org/jaqpot/"){
-    # Get jaqpot username and password
-    username <- readline("Username: ")
-    password <- getPass::getPass(msg = "Password: ", noblank = FALSE, forcemask = FALSE)
-    loginto <- paste(url, "services/aa/login/", sep = "")
-    body <- list(username=username, password = password)
-    httr::set_config(httr::config(ssl_verifypeer = 0L))
-    
-    tryCatch({
+  # Get jaqpot username and password
+  username <- readline("Username: ")
+  password <- getPass::getPass(msg = "Password: ", noblank = FALSE, forcemask = FALSE)
+  loginto <- paste(url, "services/aa/login/", sep = "")
+  body <- list(username=username, password = password)
+  httr::set_config(httr::config(ssl_verifypeer = 0L))
+  
+  tryCatch({
     res <-  httr::POST(loginto, body = body, encode = "form")
     stopifnot(httr::status_code(res) < 300)
     res <- httr::content(res, "text", encoding = 'UTF-8')
     authResponse <- jsonlite::fromJSON(res)
     #globally define token to be seen by other jaqpot functions 
     jaqpot.token <<- authResponse$authToken
-    }, error = function(e) {
-          e$message <-"http call failed. Make sure you provided the correct username and password."
-          stop(e)
-    })
+    print("Token created")
+  }, error = function(e) {
+    e$message <-"http call failed. Make sure you provided the correct username and password."
+    stop(e)
+  })
 }
-    
-    
+
+
 #' Jaqpot login using credentials without masking
 #'
 #' Provides the user the ability to communicate with the jaqpot server
@@ -51,25 +52,27 @@ login.cred.safe <- function(url = "https://api.jaqpot.org/jaqpot/"){
 
 
 login.cred <- function(username, password, url = "https://api.jaqpot.org/jaqpot/"){
-    loginto <- paste(url, "services/aa/login/", sep = "")
-    body <- list(username=username, password = password)
-    httr::set_config(httr::config(ssl_verifypeer = 0L))
-    
-    tryCatch({
+  loginto <- paste(url, "services/aa/login/", sep = "")
+  body <- list(username=username, password = password)
+  httr::set_config(httr::config(ssl_verifypeer = 0L))
+  
+  tryCatch({
     res <-  httr::POST(loginto, body = body, encode = "form")
     stopifnot(httr::status_code(res) < 300)
     res <- httr::content(res, "text", encoding = 'UTF-8')
     authResponse <- jsonlite::fromJSON(res)
     #globally define token to be seen by other jaqpot functions 
     jaqpot.token <<- authResponse$authToken
-    }, error = function(e) {
-          e$message <-"http call failed. Make sure you provided the correct username and password."
-          stop(e)
-    })
-}    
+    print("Token created")
     
-  
- #' Jaqpot login using api key with masking
+  }, error = function(e) {
+    e$message <-"http call failed. Make sure you provided the correct username and password."
+    stop(e)
+  })
+}    
+
+
+#' Jaqpot login using api key with masking
 #'
 #' Provides the user the ability to communicate with the jaqpot server
 #' 
@@ -84,22 +87,24 @@ login.cred <- function(username, password, url = "https://api.jaqpot.org/jaqpot/
 #' 
 #' @export 
 login.api.safe <- function(url = "https://api.jaqpot.org/jaqpot/"){
- 
-    tryCatch({
-      API_key <- getPass::getPass(msg = "API Key: ", noblank = FALSE, forcemask = FALSE)
-      loginto <- paste(basepath, "services/aa/validate/accesstoken", sep = "")
-      httr::set_config(httr::config(ssl_verifypeer = 0L))
-      res <-  httr::POST(loginto, body = API_key)
-      stopifnot(httr::status_code(res) < 300)
-      jaqpot.token <<- API_key
-    }, error = function(e) {
-      e$message <-"http call failed. Make sure you provided the correct API key."
-      stop(e)
-    }) 
+  
+  tryCatch({
+    API_key <- getPass::getPass(msg = "API Key: ", noblank = FALSE, forcemask = FALSE)
+    loginto <- paste(url, "services/aa/validate/accesstoken", sep = "")
+    httr::set_config(httr::config(ssl_verifypeer = 0L))
+    res <-  httr::POST(loginto, body = API_key)
+    stopifnot(httr::status_code(res) < 300)
+    jaqpot.token <<- API_key
+    print("Token created")
+    
+  }, error = function(e) {
+    e$message <-"http call failed. Make sure you provided the correct API key."
+    stop(e)
+  }) 
 }
 
 
- #' Jaqpot login using api key 
+#' Jaqpot login using api key 
 #'
 #' Provides the user the ability to communicate with the jaqpot server
 #' 
@@ -113,15 +118,17 @@ login.api.safe <- function(url = "https://api.jaqpot.org/jaqpot/"){
 #'
 #' @export 
 login.api <- function(API_key, url = "https://api.jaqpot.org/jaqpot/"){
- 
-    tryCatch({
-      loginto <- paste(url, "services/aa/validate/accesstoken", sep = "")
-      httr::set_config(httr::config(ssl_verifypeer = 0L))
-      res <-  httr::POST(loginto, body = API_key)
-      stopifnot(httr::status_code(res) < 300)
-      jaqpot.token <<- API_key
-    }, error = function(e) {
-      e$message <-"http call failed. Make sure you provided the correct API key."
-      stop(e)
-    }) 
+  
+  tryCatch({
+    loginto <- paste(url, "services/aa/validate/accesstoken", sep = "")
+    httr::set_config(httr::config(ssl_verifypeer = 0L))
+    res <-  httr::POST(loginto, body = API_key)
+    stopifnot(httr::status_code(res) < 300)
+    jaqpot.token <<- API_key
+    print("Token created")
+    
+  }, error = function(e) {
+    e$message <-"http call failed. Make sure you provided the correct API key."
+    stop(e)
+  }) 
 }
