a6b33e34d97cb9028110c51e97785bf17046dab7
alarv <alarvfm@gmail.com>
2024-11-19 16:35:55 +0200

    fix: current state

--- R/upload_model.R
+++ R/upload_model.R
@@ -1,7 +1,13 @@
 # somehow import
 source('../openapi/R')
 
-model_api <- ModelApi$new()
+read('Give me api key')
+read('Give me api secret')
+
+api_client = ApiClient$new(default_headers = list(`X-Api-Key` = env$JAQPOT_API_KEY, `X-Api-Secret` = env$JAQPOT_API_SECRET))
+model_api <- ModelApi$new(api_client = api_client)
 # Example usage to create a model
 response <- model_api$CreateModel(rawModel = rawModel)
 print(response)
+
+
