567629c58229b180dc19c2f481581dd0024bef4c
alarv <alarvfm@gmail.com>
2024-11-19 15:47:00 +0200

    fix: current state of upload_model.R

--- R/upload_model.R
+++ R/upload_model.R
@@ -1,6 +1,7 @@
-library('../openapi')
+# somehow import
+source('../openapi/R')
 
 model_api <- ModelApi$new()
 # Example usage to create a model
-response <- model_api$CreateModel()
+response <- model_api$CreateModel(rawModel = rawModel)
 print(response)
