d9af5d8596e1afe9738570fc9b59fa8c66b37de5
Periklis_ts <ptsirostsib@gmail.com>
2024-11-24 14:34:24 +0200

    feat: examples of usage

---
+++ examples/example_jaqpot_predict.R
@@ -0,0 +1,12 @@
+envFile <- "jaqpot.env"
+modelID = "1933"
+df <-list(
+  "weight" = 80,
+  "dose" = 10,
+  "administration.time" = 0.1,
+  "sim.start" = 0,
+  "sim.step" = 1,
+  "sim.stop" = 10
+)
+
+jaqpot.predict( df, modelID, url = "https://api.jaqpot.org/", envFile =envFile)
