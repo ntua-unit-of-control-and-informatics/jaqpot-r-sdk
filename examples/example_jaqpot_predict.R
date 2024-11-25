a2bc954cde5e93f31af7e63c41603b1241a8dead
Periklis_ts <ptsirostsib@gmail.com>
2024-11-25 14:20:42 +0200

    feat: add an example of float_array

--- examples/example_jaqpot_predict.R
+++ examples/example_jaqpot_predict.R
@@ -10,3 +10,24 @@ df <-list(
 )
 
 jaqpot.predict( df, modelID, url = "https://api.jaqpot.org/", envFile =envFile)
+
+PFOS_rat_pbk_modelID = "1557"  # Loccisano et al. 2012
+# Seacat et al. 2002 - PFOS exposure - LOAEL
+pfos_bw <- 0.282  # kg
+pfos_daily_intake_per_kg <- 1  # mg/kg/day
+pfos_daily_intake_per_rat <- pfos_daily_intake_per_kg * pfos_bw  # mg/day
+pfos_exposure_duration <- 14 * 7  # days
+
+exposure_scenario <- list("admin.type"= "oral",
+                        "admin.dose"= list(rep(pfos_daily_intake_per_rat, pfos_exposure_duration)),
+                        "admin.time"= list(seq(0, pfos_exposure_duration * 24-1, 24)),
+                         "BW"= 0.282,
+                         "BW.times"= 0,
+                        "F_unabs"= 0.0,
+                        "sex"= "M",
+                        "sim.start"= 0,
+                        "sim.end"= pfos_exposure_duration * 24,
+                        "sim.step"= 1)
+
+jaqpot.predict( df = exposure_scenario, modelID = PFOS_rat_pbk_modelID, 
+                url = "https://api.jaqpot.org/", envFile =envFile)
